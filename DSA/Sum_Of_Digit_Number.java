e2fe471357f4b07b63bc8b18195469ee30d5a843
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2023-10-18 15:42:56 +0600

    sum of digit

---
+++ DSA/Sum_Of_Digit_Number.java
@@ -0,0 +1,25 @@
+package DSA;
+
+public class Sum_Of_Digit_Number {
+    static int getSum(int n)
+    {
+        int sum = 0;
+
+
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int n = 687;
+
+        // Function call
+        System.out.println(getSum(n));
+    }
+}
